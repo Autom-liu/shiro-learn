27e78bab8c229ecf0a6f699a3278e2bb8b082f7c
Autom-liu <349576362@163.com>
2019-04-27 21:45:47 +0800

    登录认证逻辑
    Signed-off-by: Autom-liu <349576362@163.com>

--- src/main/java/com/edu/scnu/web/controller/UserController.java
+++ src/main/java/com/edu/scnu/web/controller/UserController.java
@@ -1,6 +1,10 @@
 package com.edu.scnu.web.controller;
 
 import com.edu.scnu.bean.User;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -23,7 +27,13 @@ public class UserController {
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
     public String login(User user) {
-
+        Subject subject = SecurityUtils.getSubject();
+        UsernamePasswordToken token = new UsernamePasswordToken(user.getUsername(), user.getPassword());
+        try {
+            subject.login(token);
+        } catch (AuthenticationException e) {
+            return "redirect:/user/login";
+        }
         return "redirect:/sys/index";
     }
 }
